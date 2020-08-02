96b5c1e52af287dcdce4b592913abf07f9d255fd
ponsata <khr.04237@gmail.com>
2020-08-02 16:18:48 +0900

    1.ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -15,4 +16,7 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday ])
+  end
 end
