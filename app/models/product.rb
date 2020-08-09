7f420756b8e2802848df53448b42e8ff3785bc91
daikifujita <d.fujita21@gmail.com>
2020-08-09 16:29:38 +0900

    商品モデルの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+
+  belongs_to :saler, class_name: "User", dependent: :destroy
+  belongs_to :buyer, class_name: "User", dependent: :destroy
+  belongs_to :category
+  has_many :pictures, dependent: :destroy
+
+  enum condition: { 新品、未使用: 0, 未使用に近い:1, 目立った傷や汚れなし: 2, やや傷や汚れあり: 3, 傷や汚れあり: 4 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
 end
