021284db0a030d5a7aca976ae28a8491e5226660
daikifujita <d.fujita21@gmail.com>
2020-08-10 20:22:30 +0900

    productモデル修正、userモデル微修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   belongs_to :saler, class_name: "User", optional: true, dependent: :destroy
   belongs_to :buyer, class_name: "User", optional: true, dependent: :destroy
   belongs_to :category
-  has_many :pictures
+  has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures, allow_destroy: true
 
   enum condition: { unused: 0, near_unused: 1, no_noticeable_scratches_dirt: 2, some_scratches_dirt: 3, scratches_dirt: 4 }
