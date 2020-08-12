673ecc185ecddbed73605dcb62ceb74d5c2d79de
daikifujita <d.fujita21@gmail.com>
2020-08-12 13:01:07 +0900

    カテゴリーを番号入力ではなく、プルダウンから選択できるように実装
    1. ancestryを利用
    2. 親カテゴリーを初期値に戻すとエラ〜メッセージが出る不具合がまだ残っている。後ほど修正予定。
    複数の過去作った人のを参考にし、ようやく完成。
    https://qiita.com/manbolila/items/7c44142de50093470580
    https://qiita.com/hukuro310/items/1843d22b4ea29c2b1692
    https://qiita.com/kaito_program/items/3755f071625f9f5bbaba
    https://qiita.com/renaboo/items/e9ad2a1d2033cc23c0d8

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,9 @@ class ProductsController < ApplicationController
     if user_signed_in?
       @product = Product.new
       @product.pictures.new
+      @category_parent_array =  Category.where(ancestry: nil) do |parent|
+        @category_parent_array << parent
+      end
     else
       redirect_to root_path
     end
@@ -33,6 +36,18 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(id: params[:parent_name]).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  
   private
 
   def product_params
