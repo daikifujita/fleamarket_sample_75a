83621778590f7886486e8d45bacbf53317dfcbf8
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-09-03 01:49:02 +0900

    Top-page
    Top-page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,9 @@
 class TopController < ApplicationController
+  def index
+    @product = Product.all
+    @pictures = Picture.all
+    @category_parent_array =  Category.where(ancestry: nil) do |parent|
+      @category_parent_array << @category_parent_array =  Category.where(ancestry: nil)
+    end
+  end
 end
