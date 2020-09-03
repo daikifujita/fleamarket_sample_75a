c601460fffddf5fc12b68e5439934e32a634c24d
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-09-03 12:07:07 +0900

    top-page(revison1)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,6 @@ class TopController < ApplicationController
   def index
     @product = Product.all
     @pictures = Picture.all
-    @category_parent_array =  Category.where(ancestry: nil) do |parent|
-      @category_parent_array << @category_parent_array =  Category.where(ancestry: nil)
-    end
+    @category_parent_array =  Category.where(ancestry: nil)
   end
 end
