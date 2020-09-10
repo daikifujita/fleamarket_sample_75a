41106cc0a90340125863e1e4072391619dde43f0
ponsata <khr.04237@gmail.com>
2020-09-10 22:02:41 +0900

    ログイン確認画面修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,35 +1,39 @@
 class PurchaseController < ApplicationController
   
   require "payjp"
+  before_action :authenticate_user!
   before_action :set_card 
   before_action :set_product   #クレジットカードと製品の変数を設定
   before_action :set_payjp_secretkey
 
   def index
+    if @products.buyer_id.presents?
+      @address = Address.where(user_id: current_user.id).first
+      #Payjpから顧客情報を取得し、表示
+      if @card.present?
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        @card_information = customer.cards.retrieve(@card.card_id)
+        @card_brand = @card_information.brand 
+        case @card_brand
+        when "Visa"
+          @card_src = "visa.png"
+        when "JCB"
+          @card_src = "jcb.png"
+        when "MasterCard"
+          @card_src = "master-card.png"
+        when "American Express"
+          @card_src = "american_express.png"
+        when "Diners Club"
+          @card_src = "dinersclub.png"
+        when "Discover"
+          @card_src = "discover.png"
+        end
+      
+      else 
     
-    @address = Address.where(user_id: current_user.id).first
-    #Payjpから顧客情報を取得し、表示
-    if @card.present?
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @card_information = customer.cards.retrieve(@card.card_id)
-      @card_brand = @card_information.brand 
-      case @card_brand
-      when "Visa"
-        @card_src = "visa.png"
-      when "JCB"
-        @card_src = "jcb.png"
-      when "MasterCard"
-        @card_src = "master-card.png"
-      when "American Express"
-        @card_src = "american_express.png"
-      when "Diners Club"
-        @card_src = "dinersclub.png"
-      when "Discover"
-        @card_src = "discover.png"
-      end
-    
-    else 
-    
+      end  
+    else
+      
     end  
   end
 
