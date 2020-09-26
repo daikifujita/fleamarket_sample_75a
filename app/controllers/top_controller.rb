461a60895807a2ee142444c9b70d985420ef84fb
daikifujita <d.fujita21@gmail.com>
2020-09-26 09:57:24 +0900

    新規商品並び順変更、出品中商品一覧表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
-    @products = Product.all.includes(:pictures)
+    @products = Product.all.includes(:pictures).order('created_at DESC')
+    @saling_products = Product.where(buyer_id: nil).includes(:pictures).order('created_at DESC')
     @category_parent_array =  Category.where(ancestry: nil)
   end
 end
