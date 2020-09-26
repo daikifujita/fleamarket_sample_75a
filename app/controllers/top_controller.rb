40e91ce5fa6c75668524629f49134a40b160a32e
daikifujita <d.fujita21@gmail.com>
2020-09-26 10:12:25 +0900

    all削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @products = Product.all.includes(:pictures).order('created_at DESC')
+    @products = Product.includes(:pictures).order('created_at DESC')
     @saling_products = Product.where(buyer_id: nil).includes(:pictures).order('created_at DESC')
     @category_parent_array =  Category.where(ancestry: nil)
   end
