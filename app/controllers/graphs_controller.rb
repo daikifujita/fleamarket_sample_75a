71b22e9078e5242a12d27135747b1c3e7e151016
daikifujita <d.fujita21@gmail.com>
2020-09-28 21:51:43 +0900

    gon導入

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,12 +4,25 @@ class GraphsController < ApplicationController
     @products = Product.where(saler_id: current_user).where.not(buyer_id: nil)
 
     today = Date.today
+    @sold_6days_ago_products = @products.where(tradeday: today.ago(6.days))	
+    @sold_5days_ago_products = @products.where(tradeday: today.ago(5.days))	
+    @sold_4days_ago_products = @products.where(tradeday: today.ago(4.days))	
+    @sold_3days_ago_products = @products.where(tradeday: today.ago(3.days))	    
     @sold_2days_ago_products = @products.where(tradeday: today.ago(2.days))	
     @sold_1days_ago_products = @products.where(tradeday: today.ago(1.days))	
     @sold_today_products = @products.where(tradeday: today)
     
-    @total_amount = []
-    @total_amount << @sold_2days_ago_products.all.sum(:price)
+    @total_amounts = []
+
+    @total_amounts << @sold_6days_ago_products.all.sum(:price)
+    @total_amounts << @sold_5days_ago_products.all.sum(:price)
+    @total_amounts << @sold_4days_ago_products.all.sum(:price)
+    @total_amounts << @sold_3days_ago_products.all.sum(:price)
+    @total_amounts << @sold_2days_ago_products.all.sum(:price)
+    @total_amounts << @sold_1days_ago_products.all.sum(:price)
+    @total_amounts << @sold_today_products.all.sum(:price)
+
+    gon.total_amounts = @total_amounts
 
   end
   
